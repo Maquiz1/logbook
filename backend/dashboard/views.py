5061ff20188a0c2ed253f871d89521829f79e42f
Maquiz1 <manquiz92@gmail.com>
2025-07-30 13:31:57 +0300

    first commit

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -2,5 +2,24 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # @login_required
+# def user_dashboard(request):
+#     return render(request, 'dashboard/home.html', {'user': request.user})
+
+
+from django.shortcuts import render
+from locations.models import Country
+from clinical.models import Disease
+from mentorship.models import Visit
+
 def user_dashboard(request):
-    return render(request, 'dashboard/home.html', {'user': request.user})
+    country_count = Country.objects.count()
+    disease_count = Disease.objects.count()
+    visit_count = Visit.objects.count()
+    recent_visits = Visit.objects.select_related('site', 'mentor').order_by('-start_date')[:5]
+
+    return render(request, 'dashboard/home.html', {
+        'country_count': country_count,
+        'disease_count': disease_count,
+        'visit_count': visit_count,
+        'recent_visits': recent_visits,
+    })
