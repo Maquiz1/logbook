96055c727de8d8b95f706f847298fea02bc09c59
Maquiz1 <manquiz92@gmail.com>
2025-07-30 16:59:13 +0300

    adds

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
-# @login_required
-# def user_dashboard(request):
-#     return render(request, 'dashboard/home.html', {'user': request.user})
-
-
 from django.shortcuts import render
 from locations.models import Country
 from clinical.models import Disease
 from mentorship.models import Visit
 
+
+@login_required
 def user_dashboard(request):
     country_count = Country.objects.count()
     disease_count = Disease.objects.count()
@@ -22,4 +18,5 @@ def user_dashboard(request):
         'disease_count': disease_count,
         'visit_count': visit_count,
         'recent_visits': recent_visits,
+        'user': request.user
     })
