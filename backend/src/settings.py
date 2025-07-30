a06bc5036e09632e8dfdfc7711f66be0329da02e
Maquiz1 <manquiz92@gmail.com>
2025-07-30 14:26:08 +0300

    fix

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -115,6 +115,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Location to collect static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional directories to look for static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Optional shared static folder
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
