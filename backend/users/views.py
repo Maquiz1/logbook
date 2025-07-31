6ac8947275bd7af1aca0840cacbfba12bcedb434
Maquiz1 <manquiz92@gmail.com>
2025-07-31 04:29:00 +0300

    fix

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,5 +6,5 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpView(CreateView):
     model = User
     form_class = UserCreationForm
-    template_name = 'users/registraion/sign_up.html'
+    template_name = 'users/registration/sign_up.html'
     success_url = reverse_lazy('dashboard:dashboard')  # redirect here after signup
